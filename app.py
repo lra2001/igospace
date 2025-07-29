eaad5be0f803c1a84d6c8db37712e2eb819c8b7e
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-07-29 11:12:16 +0100

    Update app.py
    - remove commented code
    - remove sample route

--- app.py
+++ app.py
@@ -10,26 +10,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 # initialize the app with the extension
 db.init_app(app)
 
+# create routes
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# Create a simple route
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Database connected (if no errors)!'
+    return render_template('home.html', active_page='home')
 
-# @app.route("/")
-# def home():
-#     return render_template('home.html', active_page='home')
+@app.route('/register')
+def register():
+    return render_template('register.html', active_page='register')
 
-# @app.route('/register')
-# def register():
-#     return render_template('register.html', active_page='register')
-
-# @app.route('/login')
-# def login():
-#     return render_template('login.html', active_page='login')
+@app.route('/login')
+def login():
+    return render_template('login.html', active_page='login')
 
 # Create tables for models
 @app.before_request
