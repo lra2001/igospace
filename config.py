0c29c213c3dfd6bd4ffd3606501cbed44bd3183e
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-07-30 20:40:44 +0100

    commenting code for prod

--- config.py
+++ config.py
@@ -4,10 +4,18 @@ import os
 load_dotenv()
 
 # DB credentials
-DB_USER = 'admin'
-DB_PASSWORD = 'testpwd!'
-DB_HOST = 'localhost'
-DB_NAME = 'igospace'
+# DB_USER = 'admin'
+# DB_PASSWORD = 'testpwd!'
+# DB_HOST = 'localhost'
+# DB_NAME = 'igospace'
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+# SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
+
+# Update the SQLALCHEMY_DATABASE_URI to use environment variables
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://admin:testpwd!@localhost/igospace"
+)
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
