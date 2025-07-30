5effd7e0477ceb4e10700427740b6ec6f3518ce8
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-07-30 23:48:11 +0100

    update render and config

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import os
 load_dotenv()
 
 # Update the SQLALCHEMY_DATABASE_URI to use environment variables
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://admin:testpwd!@localhost:5432/igospace"
+)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
